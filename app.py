6c2b34e11ef820e7b32c9bab2168130e3fd284eb
Jose Serrano <josejasm.15@gmail.com>
2024-05-02 16:58:03 -0400

    fc:tes-backend

--- app.py
+++ app.py
@@ -5,8 +5,68 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/users', methods=['GET'])
+def hack_1():
+    return jsonify({'payload': 'success'}), 200
 
+@app.route('/user', methods=['POST'])
+def hack2():
+    return jsonify ({'payload':'success'}), 200
 
+@app.route('/user', methods=['DELETE'])
+def hack3():
+    return jsonify ({'payload':'success'}), 200
+
+@app.route('/user', methods=['PUT'])
+def hack4():
+    return jsonify ({'payload':'success', 'error':False},), 200
+
+@app.route('/api/v1/users', methods=['GET'])
+def hack5():
+    return jsonify ({'payload':[]}), 200
+
+@app.route('/api/v1/user', methods=['POST'])
+def hack6():
+
+    email = request.args.get('email')
+    name = request.args.get('name')
+
+    return jsonify ({'payload': {
+        'email': email,
+        'name': name
+    }}), 200
+
+@app.route('/api/v1/user/add', methods=['POST'])
+def hack7():
+
+    data = request.form
+    email = data.get('email')
+    name = data.get('name')
+    id = data.get('id')
+
+    return jsonify({
+        'payload':{
+            'email':email,
+            'name':name,
+            'id':id
+        }
+    })
+
+@app.route('/api/v1/user/create', methods=['POST'])
+def hack8():
+
+    data = request.get_json()
+    email = data.get('email')
+    name = data.get('name')
+    id = data.get('id')
+
+    return jsonify({
+        'payload':{
+            'email':email,
+            'name':name,
+            'id':id
+        }
+    })
 
 
 if __name__ == "__main__":
